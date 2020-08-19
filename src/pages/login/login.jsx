4c047a254f98e1e85656f32b4942b133aa5d8cde
âAsahi-sun <1596014525@qq.com>
2020-08-19 16:11:27 +0800

    note

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import {
+    Form,
+    Input,
+    Icon,
+    Button,
+    Checkbox
+} from 'antd';
+
+import {
+    UserOutlined,
+    LockOutlined
+} from '@ant-design/icons';
+
+
 import logo from './images/logo.png';
 import './login.less';
 
 class Login extends Component {
+
+
 
     render() {
         return (
@@ -13,7 +29,41 @@ class Login extends Component {
                 </div>
                 <div className="login-content">
                     <h1>用户登录</h1>
-                    <div> From组件 </div>
+                    <div>
+                        <Form
+                            name="normal_login"
+                            className="login-form"
+                            initialValues={{ remember: true }}
+
+                        >
+                            <Form.Item
+                                name="username"
+                                rules={[{ required: true, message: 'Please input your Username!' }]}
+                            >
+                                <Input
+                                    prefix={<UserOutlined className="site-form-item-icon" />}
+                                    placeholder="用户名" 
+                                />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your Password!' }]}
+                            >
+                                <Input.Password
+                                    prefix={<LockOutlined className="site-form-item-icon" />}
+                                    type="password"
+                                    placeholder="密码"
+                                />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" style = {{ width:'100%' }} className="login-form-button">
+                                    登录
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
                 </div>
             </div>
         )
